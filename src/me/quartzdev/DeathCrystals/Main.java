63ac0630adf3d7f852b7a75834805a561a906753
rockhopper72 <stevenk72@gmail.com>
2015-07-31 02:42:30 -0400

    Add main class

---
+++ src/me/quartzdev/DeathCrystals/Main.java
@@ -0,0 +1,15 @@
+package me.quartzdev.DeathCrystals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+
+}
