40c34a5e8b4f6493969a127ce53f185763c708ff
rockhopper72 <stevenk72@gmail.com>
2015-07-31 03:09:08 -0400

    Add crystal file manager

---
+++ src/me/quartzdev/DeathCrystals/Crystals/CrystalFileManager.java
@@ -0,0 +1,13 @@
+package me.quartzdev.DeathCrystals.Crystals;
+
+public class CrystalFileManager {
+	
+	public void save() {
+		// TODO: Under Construction
+	}
+	
+	public void load() {
+		// TODO: Under Construction
+	}
+
+}
--- src/me/quartzdev/DeathCrystals/Main.java
+++ src/me/quartzdev/DeathCrystals/Main.java
@@ -2,15 +2,22 @@ package me.quartzdev.DeathCrystals;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import me.quartzdev.DeathCrystals.Crystals.CrystalFileManager;
+
 public class Main extends JavaPlugin {
 	
+	private CrystalFileManager crystalFileManager;
+	
 	public void onEnable() {
+		this.crystalFileManager = new CrystalFileManager();
+		this.crystalFileManager.load();
+		
 		PlayerDeathListener playerDeathListener = new PlayerDeathListener();
 		this.getServer().getPluginManager().registerEvents(playerDeathListener, this);
 	}
 	
 	public void onDisable() {
-		
+		this.crystalFileManager.save();
 	}
 
 }
