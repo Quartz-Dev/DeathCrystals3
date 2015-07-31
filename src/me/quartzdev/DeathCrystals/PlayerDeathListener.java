538119b545dfa5fec8f1852bc2c552f3afe01677
rockhopper72 <stevenk72@gmail.com>
2015-07-31 03:08:54 -0400

    Update docs

--- src/me/quartzdev/DeathCrystals/PlayerDeathListener.java
+++ src/me/quartzdev/DeathCrystals/PlayerDeathListener.java
@@ -7,6 +7,12 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlayerDeathListener implements Listener {
 	
+	/**
+	 * Spawns a death crystal in the place where a player died, which is right-clickable to allow
+	 * retrieval of those items
+	 * 
+	 * @param event
+	 */
 	@EventHandler(priority = EventPriority.NORMAL)
 	public void onPlayerDeath(PlayerDeathEvent event) {
 		
